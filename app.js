33bc6e5382c91882bcfe37e01fda3128edfffb07
my name <blueshaperd3@gmail.com>
2024-02-28 03:06:24 +0500

    java starting

---
+++ app.js
@@ -0,0 +1,8 @@
+var passward = ("Error! Please enter a valid passward.");
+alert (passward);
+
+var land = ("Welcome to JS Land...\n Happy Coding!");
+alert (land);
+
+var js = ("Welcome to JS Land");
+alert (js);
