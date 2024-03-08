574803993e30229d64f26180bfffbd05c8c724b3
my name <blueshaperd3@gmail.com>
2024-03-09 00:28:58 +0500

    assingment 5

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
 //Assingment 1
-//var age = 15;
-//alert("I am 41 years old")
-//alert("I am visited 14 times")
-
-//var birth = 1990;
-//document.write("My birth year is 1982")
-
-var visitorsName = prompt ("Enter your name")
-var productTitle = prompt ("Enter the product title")
-var quantity = prompt ("Enter the quantity")
-var message = (visitorsName + " " + "ordered" + " " + quantity + " " + productTitle + " " + "(s) on XYZ Cloting Store");{
-    document.write(message)
-}
-
-
 
 //var passward = ("Error! Please enter a valid passward.");
 //alert (passward);
@@ -29,3 +14,45 @@ var message = (visitorsName + " " + "ordered" + " " + quantity + " " + productTi
 
 //var consol = "Hello! I can run JS through my web browser's console";
 //console.log (consol);
+
+
+
+//Assingment 3
+
+//var age = 15;
+//alert("I am 41 years old")
+//alert("I am visited 14 times")
+
+//var birth = 1990;
+//document.write("My birth year is 1982")
+
+//var visitorsName = prompt ("Enter your name")
+//var productTitle = prompt ("Enter the product title")
+//var quantity = prompt ("Enter the quantity")
+//var message = (visitorsName + " " + "ordered" + " " + quantity + " " + productTitle + " " + "(s) on XYZ Cloting Store");{
+//    document.write(message)
+//}
+
+
+//Assingment 5
+
+//var a = 3;
+//var b = 5;
+//var c = a+b;
+//document.write(c)
+
+//var a = 3;
+//var b = 5;
+//var c = b-a;
+//document.write(c)
+
+//var a = 3;
+//var b = 5;
+//var c = a*b;
+//document.write(c)
+
+//var a = 3;
+//var b = 5;
+//var c = b/a;
+//document.write(c)
+
