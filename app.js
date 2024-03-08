5e6e603007a4be98019a928354bcf4b2db59ad02
my name <blueshaperd3@gmail.com>
2024-03-08 20:51:11 +0500

    assingment 1 done

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
-var passward = ("Error! Please enter a valid passward.");
-alert (passward);
+//Assingment 1
+//var age = 15;
+//alert("I am 41 years old")
+//alert("I am visited 14 times")
 
-var land = ("Welcome to JS Land...\n Happy Coding!");
-alert (land);
+//var birth = 1990;
+//document.write("My birth year is 1982")
 
-var js = ("Welcome to JS Land");
-alert (js);
+var visitorsName = prompt ("Enter your name")
+var productTitle = prompt ("Enter the product title")
+var quantity = prompt ("Enter the quantity")
+var message = (visitorsName + " " + "ordered" + " " + quantity + " " + productTitle + " " + "(s) on XYZ Cloting Store");{
+    document.write(message)
+}
 
-var coding = ("Happy Coding!\n Prevent this page from creating additional dialogs");
-alert (coding);
 
-var consol = "Hello! I can run JS through my web browser's console";
-console.log (consol);
+
+//var passward = ("Error! Please enter a valid passward.");
+//alert (passward);
+
+//var land = ("Welcome to JS Land...\n Happy Coding!");
+//alert (land);
+
+//var js = ("Welcome to JS Land");
+//alert (js);
+
+//var coding = ("Happy Coding!\n Prevent this page from creating additional dialogs");
+//alert (coding);
+
+//var consol = "Hello! I can run JS through my web browser's console";
+//console.log (consol);
